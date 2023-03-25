f0c2e7e17a144d55a07e327c5c98bf134c404bf9
WahomeVincentMwangiclear <vinwahome@gmail.com>
2023-03-25 19:45:47 +0300

    First test

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Your code goes here
+document.addEventListener('DOMContentLoaded', function() {
+    console.log("DOM content loaded")
+})
+
+console.log("This console log fires when the JS file has loaded. Before the DOMContentLoaded.")
